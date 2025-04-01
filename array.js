2e7ceabeae4b17216758f336d8d068614ef96318
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2025-04-01 13:10:44 +0530

    new

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
-console.log("HELLO");
+const arr1 = [1,2,3,4]
+// console.log(arr1.length);
+
+// =================
+
+const arr2 = [1, 2, 3, 4];
+
+arr2.pop();
+arr2.pop();
+arr2.push(3,4)
+arr2.shift()
+arr2.shift()
+arr2.unshift(1)
+arr2.unshift(2)
+
+// console.log("arr2", arr2);
+
+// =================
+const arr3 = [1,2,3,4]
+
+for(let i = 0; i < arr3.length ; i++){
+    console.log(arr3[i]);
+}
+
+// =================
+const arr4 = [1,2,3,4]
+
+let i = 0
+
+while(i < arr4.length){
+    console.log(arr4[i]);
+    i++
+}
---
+++ arrayInbuiltLoop_map.js
@@ -0,0 +1,29 @@
+const arr1 = [1, 2, 3, 4, 5];
+
+let numberMap = arr1.map((e) => e * e);
+
+// console.log("numberMap: ", numberMap);
+
+// =================
+const arr2 = [1, 2, 3, 4, 5];
+
+const fun2 = (arr2) => {
+  return arr2.map((e) => e * e);
+};
+
+// console.log("arr2", fun2(arr2));
+
+// =================
+
+const arr3 = [1, 2, 3, 4, 5];
+
+const fun3 = (arr3) => {
+  return arr3.map((e) => {
+    console.log("e: ", e);
+    return e * e;
+  });
+};
+
+// console.log("arr3", fun3(arr3));
+
+// =================
