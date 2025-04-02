d7183085b042e946e2ca27e5e4178d588c66a8e1
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2025-04-02 14:51:23 +0530

    new

--- arrayTest.js
+++ arrayTest.js
@@ -73,3 +73,85 @@ const q7 = (products) => {
 }
 // console.log('q7: ', q7(products));
 
+// Q8.Retrieve all products that have a price greater than 500.
+
+const q8 = (products) =>{
+    return products.filter((product)=>product.price > 500)
+}
+
+// console.log("q8", q8(products));
+
+// Q9.Find products that are in stock but have a price less than 300.
+
+const q9 = (products) =>{
+    return products.filter((product)=> product.stock > 0 && product.price < 300)
+}
+// console.log('q9: ', q9(products));
+
+// Q10.Extract all products from Accessories category that are priced above 100.
+
+const q10 = (products)=>{
+    return products.filter((product)=> product.category === "Accessories" && product.price > 100 )
+}
+// console.log('q10: ', q10(products));
+
+// ==============================================REDUCE==============================================
+
+// Q11.Find the total number of units in stock for all products.
+
+const q11 = (products) =>{
+    return products.reduce((prev,curr)=>curr.stock+prev,0)
+}
+// console.log('q11: ', q11(products));
+
+// Q12. Determine the most expensive product.
+
+const q12 = (products) =>{
+    return products.reduce((max,product)=>product.price > max.price ? product : max,products[0] )
+}
+// console.log('q12: ', q12(products));
+
+// Q13.Find the cheapest product.
+
+const q13 = (products) => {
+    return products.reduce((min,product)=>product.price < min.price ? product : min, products[0])
+}
+// console.log('q13: ', q13(products));
+
+// Q14.Calculate the total revenue if all available stock is sold (price * stock for each product).
+
+const q14 = (products) =>{
+    return products.reduce((prev,curr)=>curr.price*curr.stock + prev,0)
+}
+// console.log('q14: ', q14(products));
+
+// Q15.Count how many products belong to each category and return an object like:
+
+const q15 = (products) =>{
+    return products.reduce((acc,row)=>{
+        const category = row.category
+        if(acc[category]){
+            acc[category] += 1
+        }else{
+            acc[category] = 1
+        }
+
+        return acc
+    },{})
+}
+// console.log('q15: ', q15(products));
+
+// Q16.Group products by category into an object:
+
+const q16 = (products) =>{
+    return products.reduce((acc,row)=>{
+        const category = row.category
+        if(!acc[category]){
+            acc[category]=[]
+        }
+        acc[category].push(row)
+        return acc
+    },{})
+}
+console.log('q16: ', q16(products));
+
